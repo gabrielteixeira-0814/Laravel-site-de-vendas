1cb1e29290c337a2082332465aa5b3aaf83437ae
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-01-03 09:52:36 -0300

    Inserindo filds

--- app/Sale.php
+++ app/Sale.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    protected $fillable = [
+        'idUser',
+        'idProduct',
+        'valueSale',
+        'status_sales',
+        'status',
+    ];
 }
