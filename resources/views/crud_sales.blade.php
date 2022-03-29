db7566f19dc70759d3cb9655a547e41289f0cee1
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-03-28 22:28:02 -0300

    Iniciando pagina de login do sistema

--- resources/views/crud_sales.blade.php
+++ resources/views/crud_sales.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <h1> {{ $page == "edit" ? "Editar Venda" : "Adicionar" }}</h1>
+    <h1> {{ $page == "edit" ? "Editar Venda" : "Adicionar Venda" }}</h1>
     <div class='card'>
         <div class='card-body'>
 
@@ -135,9 +135,7 @@
             }
         });
     });
-// $(document).ready(function(){
-    
-// });
+
 </script>
 
 @endsection
